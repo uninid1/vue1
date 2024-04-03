3384aca12e35914bc4c84996dcdcf26cf21c4034
uninid1 <160439845+uninid1@users.noreply.github.com>
2024-04-03 17:33:08 +1000

    first

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <h1>Компонент профиля пользователя</h1>
+    <user-profile :user="user"></user-profile>
+  </div>
+</template>
+
+<script>
+import UserProfile from "./components/UserProfile.vue";
+
+export default {
+  name: "App",
+  components: {
+    UserProfile
+  },
+  data() {
+    return {
+      user: {
+        name: "uninid1",
+        avatarUrl: "https://avatar-url",
+        
+        bio: "Пользователь",
+        followers: 1,
+        following: 0
+      }
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
---
+++ src/components/UserProfile.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="user-profile">
+    <h2>{{ user.name }}</h2>
+    <img :src="user.avatarUrl" alt="User Avatar">
+    <p>{{ user.bio }}</p>
+    <p>Followers: {{ user.followers }}</p>
+    <p>Following: {{ user.following }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+.user-profile {
+  text-align: center;
+}
+
+img {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+}
+</style>
